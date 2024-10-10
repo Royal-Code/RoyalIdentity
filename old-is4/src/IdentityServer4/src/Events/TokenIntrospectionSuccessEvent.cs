7136e7e0974ec8fcc7dfdddf4de3aa781e1c4f25
Glauco Knihs <eglauko@hotmail.com>
2024-10-10 00:26:08 -0300

    refatorado is4 para rodar no .net8

--- old-is4/src/IdentityServer4/src/Events/TokenIntrospectionSuccessEvent.cs
+++ old-is4/src/IdentityServer4/src/Events/TokenIntrospectionSuccessEvent.cs
@@ -6,6 +6,7 @@ using IdentityServer4.Extensions;
 using IdentityServer4.Validation;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityServer4.Events
 {
