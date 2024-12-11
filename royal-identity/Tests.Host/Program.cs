ad776f13151d74b64a1281a5eff82ceaa9b2d7a5
Glauco Knihs <glauco@havan.com.br>
2024-12-11 18:07:40 -0300

    eod

--- royal-identity/Tests.Host/Program.cs
+++ royal-identity/Tests.Host/Program.cs
@@ -1,4 +1,5 @@
 using RoyalIdentity.Extensions;
+using RoyalIdentity.Users;
 using Tests.Host;
 
 #pragma warning disable S1118 // public Program 
@@ -16,6 +17,28 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapOpenIdConnectProviderEndpoints();
+
+app.MapPost("login", async (HttpContext context, ISignInManager signInManager) =>
+{
+    var username = context.Request.Form["username"].FirstOrDefault() ?? string.Empty;
+    var password = context.Request.Form["password"].FirstOrDefault() ?? string.Empty;
+
+    var result = await signInManager.ValidateCredentialsAsync(username, password, context.RequestAborted);
+
+    if (result.Success)
+    {
+        await signInManager.SignInAsync(result.User, result.Session, false, "pwd", context.RequestAborted);
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.Problem(
+            statusCode: 400, 
+            type: "invalid-credentials", 
+            title: "Invalid credentials",
+            detail: result.ErrorMessage);
+    }
+});
 
 app.Run();
 
