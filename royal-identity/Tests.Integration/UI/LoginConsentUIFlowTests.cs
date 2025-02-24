d9c9cd2ad7255c3410555c44a9bb9f39ccef46e9
Glauco Knihs <eglauko@hotmail.com>
2025-02-24 11:34:11 -0300

    tests for login e consent flow.

--- royal-identity/Tests.Integration/UI/LoginConsentUIFlowTests.cs
+++ royal-identity/Tests.Integration/UI/LoginConsentUIFlowTests.cs
@@ -19,6 +19,85 @@ public class LoginConsentUIFlowTests : IClassFixture<AppFactory>
     }
 
     [Fact]
+    public async Task Login_WhenValid_MustRedirectToRequestedOrigin()
+    {
+        // 1 - Login
+        // Arrange 1
+
+        var client = factory.CreateClient();
+
+        var codeVerifier = CryptoRandom.CreateUniqueId();
+        var codeVerifierBytes = Encoding.ASCII.GetBytes(codeVerifier);
+        var codeChallenge = Base64Url.Encode(codeVerifierBytes.Sha256());
+
+        var path = "/connect/authorize"
+            .AddQueryString("client_id", "demo_client")
+            .AddQueryString("response_type", "code")
+            .AddQueryString("response_mode", "query")
+            .AddQueryString("scope", "openid profile email offline_access")
+            .AddQueryString("redirect_uri", $"{client.BaseAddress}callback")
+            .AddQueryString("state", "state")
+            .AddQueryString("code_challenge", codeChallenge)
+            .AddQueryString("code_challenge_method", "S256");
+
+        // will redirect to login page
+        var loginPage = await client.GetAsync(path);
+        var content = await loginPage.Content.ReadAsStringAsync();
+        var doc = new HtmlDocument();
+        doc.LoadHtml(content);
+        var form = doc.DocumentNode.SelectSingleNode("//form");
+        var formAction = new FormAction(client, form)
+            .SetValue("Input.Username", "alice")
+            .SetValue("Input.Password", "alice");
+
+        // Act 1
+        // after login, will redirect to resource server callback
+        var response = await formAction.SubmitAsync();
+
+        // the redirect_uri will return the query string as json
+        var json = await response.Content.ReadAsStringAsync();
+        var callbackData = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+
+        // Assert 1
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(content);
+        Assert.NotNull(callbackData);
+
+        //***********************************************
+
+        // 2 - Generate Token
+
+        // Arrange 2
+        var tokenContent = new FormUrlEncodedContent(
+                new Dictionary<string, string>
+                {
+                    ["grant_type"] = "authorization_code",
+                    ["code"] = callbackData["code"],
+                    ["client_id"] = "demo_client",
+                    ["redirect_uri"] = $"{client.BaseAddress}callback",
+                    ["code_verifier"] = codeVerifier
+                });
+
+        // Act 2
+        var tokenResponse = await client.PostAsync("/connect/token", tokenContent);
+        var tokenJson = await tokenResponse.Content.ReadAsStringAsync();
+
+        // Assert 2
+        Assert.Equal(HttpStatusCode.OK, tokenResponse.StatusCode);
+        Assert.NotNull(tokenJson);
+
+        var tokenEndpointParameters = JsonSerializer.Deserialize<TokenEndpointParameters>(tokenJson);
+        // must contains: access_token, token_type, expires_in, refresh_token, id_token, scope
+        Assert.NotNull(tokenEndpointParameters);
+        Assert.NotNull(tokenEndpointParameters.AccessToken);
+        Assert.NotNull(tokenEndpointParameters.TokenType);
+        Assert.NotEqual(0, tokenEndpointParameters.ExpiresIn);
+        Assert.NotNull(tokenEndpointParameters.RefreshToken);
+        Assert.NotNull(tokenEndpointParameters.IdentityToken);
+        Assert.NotNull(tokenEndpointParameters.Scope);
+    }
+
+    [Fact]
     public async Task Login_WhenValid_MustShowConsent_WhenClientRequires_ThenRedirectToRequestedOrigin()
     {
         // 1 - Login and Consent
